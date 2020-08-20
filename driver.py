d67bebece3af45aca452a17385676b9093df5082
unknown <joseph.scalera@student.shu.edu>
2020-08-19 20:46:33 -0400

    8/19

--- driver.py
+++ driver.py
@@ -1,20 +1,36 @@
 import time
 import os
-import csv
+import requests
+import bs4
 
 from debugger import *
 
 global Kill
+Delay = 300
 
 def run():
     #Global variable to determine if we should kill the program
     global Kill
     Kill = CheckKillSwitch()#Update kill switch
 
+    LastRequest = [None]
+
 
     while (not Kill):
-        time.sleep(1)
         Kill = CheckKillSwitch()#Update kill switch
-        print(Kill)
 
+
+        html_raw = requests.get("https://www.cryptofringe.com/rss").text
+        ThisRequest = bs4.BeautifulSoup(html_raw, "html.parser")
+        #print(ThisRequest.prettify())
+
+        List = ThisRequest.channel.find_all("item")
+        for x in List:
+            title = x.title
+            print(title)
+            # description = x.description
+            # print(description)
+
+
+        time.sleep(Delay)
 run()
